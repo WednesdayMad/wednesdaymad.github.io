58a3e04ae367bdb686d83c441b6d3e4c080cad53
Wednesday <43879040+WednesdayMad@users.noreply.github.com>
2020-08-09 10:28:49 +0800

    messages update

--- message.js
+++ message.js
@@ -3,11 +3,17 @@ function generateWord()
     var words=[
       ["08/08/2020","Do you remember that time you told me about your movie idea about two otters? I wish that they have a happy ending in the sequel. I hope I changed your mind. I love you. "], 
       ["08/08/2020","I love everything about you, from your brown eyes, your long legs, your Zuko ASMR voice, your poreless skin to the way you style yourself and cut your hair. You are perfect to me."], 
-      ["08/08/2020","I hate front-end so uch Oppa. This was really hard to do. But I love you so much and you are worth all my effort and love. I hope you like my gift. I will send you mails here."], 
-      ["08/08/2020", "Thank you for all the times that you were patient and never gave up on me. I am really grateful. You are the best boyfriend any girl can ask for. I am lucky to have met you."], 
+      ["08/08/2020","I hate front-end so uch Oppa. This was really hard to do but I love you so much and you are worth all my effort and love. I hope you like my gift. I will send you mails here."], 
+      ["08/08/2020","Thank you for all the times that you were patient and never gave up on me. I am really grateful. You are the best boyfriend any girl can ask for. I am lucky to have met you."], 
       ["08/08/2020","The first time I was mad at you. You asked me to pray with you. I hope you know how much that meant to me. You keep surprising me with your thoughtfulness. Thank you. "], 
       ["08/08/2020","I'm sorry whenever I am not as understanding, patient or loveable as I should be whenever we I get mad when I don't get my way. Just know that you make me wanna stay.  "], 
-
+      ["08/09/2020","Alot of times, I feel anxious and have this urge to run away but you Always magically somehow make everything better. You said that its just luck but I think its God's blessing."], 
+      ["08/09/2020","Thank you for always spoiling me. I hope you know how much I appreciate you. Everything you do for me is full of thoughtfulness. I am beyond blessed to have met you."], 
+      ["08/09/2020","I just want you to know that its okay to be scared and not knowing where to go and what to do. Even if you are afraid of the outcome, please know I will always support you."], 
+      ["08/09/2020","I love you more than the thousands of miles between us. I love you more than the hours it takes to finally meet you. I love you more than the number of words I can ever say to you."], 
+      ["08/09/2020","Life gets hard. Its confusing and I'm always used to people leaving but at the very least I hope you become one of the few people and blessings in my life that stay in my life."], 
+      ["08/09/2020","You remind me of happy endings I thought I could never have. I'm pessimistic but you make me want to believe that fairy tales can happen in real life and that everthing will be okay."], 
+      ["08/09/2020","If you read this, I hope that we can pray together. I want to thank God for letting me meet you. I hope he blesses us and helps us move our Love into reality and into the future."], 
         ];
 
     	var random = Math.floor(Math.random() * words.length);
